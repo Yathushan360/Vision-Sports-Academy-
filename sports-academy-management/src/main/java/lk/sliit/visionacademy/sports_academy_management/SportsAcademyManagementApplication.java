6c48ac7b48ee81d90cf5d36ec78e18d5fc3078c2
Yathushan360 <IT25102785@my.sliit.lk>
2026-09-25 16:32:26 +0530

    Initial Spring Boot project structure for Billing and Payments module

---
+++ sports-academy-management/src/main/java/lk/sliit/visionacademy/sports_academy_management/SportsAcademyManagementApplication.java
@@ -0,0 +1,13 @@
+package lk.sliit.visionacademy.sports_academy_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsAcademyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsAcademyManagementApplication.class, args);
+	}
+
+}
---
+++ sports-academy-management/src/test/java/lk/sliit/visionacademy/sports_academy_management/SportsAcademyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.visionacademy.sports_academy_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsAcademyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
